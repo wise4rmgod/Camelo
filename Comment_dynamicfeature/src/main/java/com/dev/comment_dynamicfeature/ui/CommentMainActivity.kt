e3211a94f9b58a008db426d01d0b09fc770f51d0
Nwokocha wisdom maduabuchi <wise4rmgod@gmail.com>
2021-04-02 17:22:11 +0100

    added new packages

---
+++ Comment_dynamicfeature/src/main/java/com/dev/comment_dynamicfeature/ui/CommentMainActivity.kt
@@ -0,0 +1,12 @@
+package com.dev.comment_dynamicfeature.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dev.comment_dynamicfeature.R
+
+class CommentMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_comment_main)
+    }
+}
